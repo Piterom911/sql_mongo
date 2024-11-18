008454931cf22e494aa168bcb316a730396b96eb
Roman Dogoda <piterom911@gmail.com>
2024-11-18 13:29:12 +0100

    new commands -> alter, update, delete

---
+++ ls_3_alter_update_delete.sql
@@ -0,0 +1,142 @@
+CREATE DATABASE ls_3_update;
+USE ls_3_update;
+CREATE TABLE products (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(128) NOT NULL,
+    quantity INT CHECK(quantity > 0)
+);
+
+INSERt INTO products(title, quantity)
+VALUES ('Apple', 4),
+('Banana', 5),
+('Kiwi', 10),
+('Avocado', 5),
+('Mango', 2),
+('Mandarin', 12);
+
+ALTER TABLE products
+ADD COLUMN price INT DEFAULT 0;
+
+ALTER TABLE products
+MODIFY COLUMN price NUMERIC;
+
+alter table products
+change column price item_price numeric;
+
+alter table products
+drop column item_price;
+
+alter table products
+add column quality int,
+modify column quantity decimal;
+
+alter table products
+modify column quality decimal,
+change column title item_title varchar(128);
+
+alter table products
+drop column quality;
+
+create table students1 (
+	name varchar(60) not null,
+    lastname varchar(100) not null,
+    avg_mark int check(avg_mark between 0 and 5),
+    gender varchar(128) check(gender in('M', 'F'))
+);
+
+alter table students1
+add column id int primary key auto_increment first;
+
+alter table students1
+add column age int after lastname;
+
+alter table students1
+modify gender char(1);
+
+alter table students1
+change name first_name varchar(60);
+
+SELECT * FROM products;
+
+ create table employees(
+ 	employee_id int primary key auto_increment,
+ 	first_name varchar(128) not null,
+ 	last_name varchar(128) not null,
+ 	email varchar(128) not null,
+ 	salary int, 
+	department varchar(128) not null
+ );
+ 
+insert into employees(first_name, last_name, email, salary, department) values("Steven","King",	"SKING", 24000, "Sales");
+insert into employees(first_name, last_name, email, salary, department) values("Neena" , "Kochhar" , "NKOCHHAR" , 17000 , "Sales");
+insert into employees(first_name, last_name, email, salary, department) values("Lex" , "De Haan" , "LDEHAAN" , 17000 , "Sales");
+insert into employees(first_name, last_name, email, salary, department) values("Alexander" , "Hunold" , "AHUNOLD" , 9000 , "Finance");
+insert into employees(first_name, last_name, email, salary, department) values("Bruce" , "Ernst" , "BERNST" , 6000 , "Finance");
+insert into employees(first_name, last_name, email, salary, department) values("Valli" , "Pataballa" , "VPATABAL" , 4800 , "Finance");
+insert into employees(first_name, last_name, email, salary, department) values("Diana" , "Lorentz" , "DIANALO" , 8800 , "Finance");
+insert into employees(first_name, last_name, email, salary, department) values("Nancy" , "Greenberg" , "NGREENBE" , 12008 , "Shipping");
+insert into employees(first_name, last_name, email, salary, department) values("Daniel" , "Faviet" , "DFAVIET" , 9000 , "Shipping");
+insert into employees(first_name, last_name, email, salary, department) values("Jose Manuel" , "Urman" , "JMURMAN" , 7800 , "Shipping");
+insert into employees(first_name, last_name, email, salary, department) values("Luis" , "Popp" , "LPOPP" , 6900 , "Shipping");
+insert into employees(first_name, last_name, email, salary, department) values("Den" , "Raphaely" , "DRAPHEAL" , 11000 , "Marketing");
+insert into employees(first_name, last_name, email, salary, department) values("Alexander" , "Khoo" , "AKHOO" , 3100 , "Marketing");
+insert into employees(first_name, last_name, email, salary, department) values("Shelli" , "Baida" , "SBAIDA" , 2900 , "Marketing");
+insert into employees(first_name, last_name, email, salary, department) values("Sigal" , "Tobias" , "STOBIAS" , 2800 , "Marketing");
+insert into employees(first_name, last_name, email, salary, department) values("Matthew" , "Weiss" , "MWEISS" , 8000 , "Human Resources");
+insert into employees(first_name, last_name, email, salary, department) values("Adam" , "Fripp" , "AFRIPP" , 8200 , "Human Resources");
+insert into employees(first_name, last_name, email, salary, department) values("Payam" , "Kaufling" , "PKAUFLIN" , 7900 , "Human Resources");
+insert into employees(first_name, last_name, email, salary, department) values("Shanta" , "Vollman" , "SVOLLMAN" , 6500 , "Human Resources");
+insert into employees(first_name, last_name, email, salary, department) values("Kevin" , "Mourgos" , "KMOURGOS" , 5800 , "Human Resources");
+insert into employees(first_name, last_name, email, salary, department) values("Julia" , "Nayer" , "JNAYER" , 3200 , "Human Resources");
+insert into employees(first_name, last_name, email, salary, department) values("Adam" , "Markle" , "SMARKLE" , 2200 , "Human Resources");
+insert into employees(first_name, last_name, email, salary, department) values("Laura" , "Bissot" , "LBISSOT" , 3300 , "Human Resources");
+insert into employees(first_name, last_name, email, salary, department) values("Mozhe" , "Atkinson" , "MATKINSO" , 2800 , "Human Resources");
+insert into employees(first_name, last_name, email, salary, department) values("Joshua" , "Patel" , "JPATEL" , 2500 , "Human Resources");
+insert into employees(first_name, last_name, email, salary, department) values("Trenna" , "Rajs" , "TRAJS" , 3500 , "Human Resources");
+insert into employees(first_name, last_name, email, salary, department) values("John" , "Russell" , "JRUSSEL" , 14000 , "IT");
+insert into employees(first_name, last_name, email, salary, department) values("Karen" , "Partners" , "KPARTNER" , 13500 , "IT");
+insert into employees(first_name, last_name, email, salary, department) values("Alberto" , "Errazuriz" , "AERRAZUR" , 12000 , "IT");
+insert into employees(first_name, last_name, email, salary, department) values("Gerald" , "Cambrault" , "GCAMBRAU" , 11000 , "IT");
+insert into employees(first_name, last_name, email, salary, department) values("Eleni" , "Zlotkey" , "EZLOTKEY" , 10500 , "IT");
+insert into employees(first_name, last_name, email, salary, department) values("Adam" , "Vargas" , "PVARGAS" , 2500 , "Human Resources");
+insert into employees(first_name, last_name, email, salary, department) values("Laura" , "Errazuriz" , "AERRAZUR" , 12000 , "IT");
+  
+ set sql_safe_updates = 0;
+ 
+ update employees
+ set department  = "IT"
+ where first_name = "Steven" and last_name = "King";
+ 
+ update employees
+ set salary  = salary + 5000
+ where first_name = "Laura" and last_name = "Bissot";
+ 
+ update employees
+ set salary = salary * 2
+ where department = "IT";
+ 
+ update employees
+ set last_name = "Smith"
+ where first_name = "Luis" and last_name = "Popp";
+ 
+ update employees
+ set salary = salary + 5000, department = "Marketing"
+ where first_name = "John" and last_name = "Russell";
+ 
+ delete from employees
+ where first_name = "Adam" and last_name = "Fripp";
+ 
+ delete from employees
+ where department = "Shipping";
+ 
+ delete from employees
+ where employee_id in (2, 7, 10);
+ 
+ set sql_safe_updates = 1;
+ 
+select * from employees;
+
+ 
+ 
+ 
+ 
