e4a3fc016736e3dd9bd96c82801bccae17cdd0b9
Roman Dogoda <piterom911@gmail.com>
2025-02-05 19:29:37 +0100

    some changes summary 11

--- ls_11_summary.sql
+++ ls_11_summary.sql
@@ -5,12 +5,14 @@ join departments as dep
 on dep.departmentID = emp.departmentID
 group by dep.departmentID;
 
+
 -- 2. Определите количество сотрудников в каждом департаменте. Выведите название департамента и количество сотрудников.
 select dep.departmentName, count(emp.employeeID) as emp_quantity
 from employees as emp
 join departments as dep
 on dep.departmentID = emp.departmentID
 group by dep.departmentID;
+
 
 -- 3. Найдите минимальную и максимальную зарплаты в каждом департаменте. 
 select min(emp.salary) as min_salary, max(emp.salary) as max_salary
@@ -19,6 +21,7 @@ join departments as dep
 on dep.departmentID = emp.departmentID
 group by dep.departmentID;
 
+
 -- Выведите название департамента, минимальную и максимальную зарплату.
 select dep.departmentName, min(emp.salary) as min_salary, max(emp.salary) as max_salary
 from employees as emp
@@ -26,12 +29,14 @@ join departments as dep
 on dep.departmentID = emp.departmentID
 group by dep.departmentID;
 
+
 -- 4. Определите общую сумму зарплат в каждом департаменте. Выведите название департамента и общую сумму зарплат.
 select dep.departmentName, sum(emp.salary) as sum_salary
 from employees as emp
 join departments as dep
 on dep.departmentID = emp.departmentID
 group by dep.departmentID;
+
 
 -- 5. Вывести ID департамента с самой высокой средней зарплатой сотрудников.
 select dep.departmentID, avg(emp.salary) as avg_dep_salary
@@ -42,9 +47,8 @@ group by dep.departmentID
 order by avg_dep_salary desc
 limit 1;
 
+
 -- 6. Вывести имя департамента с самой высокой средней зарплатой сотрудников.
-
-
 select dep.departmentName
 from departments as dep
 where dep.departmentID = (select emp.departmentID
@@ -65,22 +69,59 @@ where emp.salary > avg_salaries.avg_salary;
 
 
 -- 8. Найдите проект с наибольшим количеством сотрудников, проработавших на нем больше 100 часов. Вывести ProjectName.
-
--- ЕЩЕ В ПРОЦЕССЕ :)
-select pj.projectName 
+select pj.projectName
 from projects as pj
-join employeeProjects as empPj
-on empPj.projectID = pj.projectID
-join (select projectID
-	from employeeProjects
-    where hoursWorked > 100) as moreThen100
-on pj.projectID =  moreThen100.projectID
-group by pj.projectID;
+join (select count(empj.employeeID) as counted, empj.projectID
+	from employeeProjects as empj
+	join (select hoursWorked, projectID, employeeID
+		from employeeProjects
+		where hoursWorked > 100) as moreThen100
+	on empj.employeeID = moreThen100.employeeID
+	group by empj.projectID) as countedEmp
+on pj.projectID = countedEmp.projectID
+order by counted desc
+limit 1;
+-- КОКОЙ ИЗ ЭТИХ ВАРИАНТОВ ЛУЧШЕ И ПОЧЕМУ?
+select p.ProjectName
+from projects as p
+join employeeprojects as ej
+on p.ProjectID = ej.ProjectID
+where HoursWorked > 100
+group by p.ProjectID
+order by count(EmployeeID) desc
+limit 1;
+
 
 -- 9. Найдите департамент с наименьшим количеством сотрудников. Выведите название департамента и количество сотрудников.
+select dep.departmentName, count(emp.employeeID) as countedEmp
+from departments as dep
+join employees as emp
+on dep.departmentID = emp.DepartmentID
+group by dep.DepartmentName
+order by countedEmp asc
+limit 1;
+
+-- НЕ ПОЛУЧИЛОСЬ РЕШИТЬ ВТОРЫМ ВАРИАНТОМ
+select  count(emp.employeeID) as counted
+from employees as emp
+join  departments as dep
+on emp.departmentID = dep.departmentID
+group by dep.DepartmentID
+having max(counted);
+
+select min(countedEmps.countAllEmps), countedEmps.departmentID
+from (select  count(emp.employeeID) as countAllEmps, emp.departmentID
+	from employees as emp
+	join  departments as dep
+	on dep.departmentID = emp.departmentID
+	group by dep.DepartmentID) as countedEmps;
+
+select d.departmentName
+from departments as d
 
 
 -- 10. Определите департамент с наибольшим суммарным бюджетом проектов, на которых работают его сотрудники.
+
 
 
 -- 10.1. Вывести ид департамента, имена и фамилии сотрудников, имена проектов и их бюджет.
